0603539df5c97b8154dd41b070a48c9abb10a592
wangjiafu5d <wangjiafu5d@163.com>
2024-05-15 16:23:55 +0800

    Initial Commit

---
+++ TencentTest2Macapp/TencentTest2MacappApp.swift
@@ -0,0 +1,17 @@
+//
+//  TencentTest2MacappApp.swift
+//  TencentTest2Macapp
+//
+//  Created by Devcloud on 2024/5/15.
+//
+
+import SwiftUI
+
+@main
+struct TencentTest2MacappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
